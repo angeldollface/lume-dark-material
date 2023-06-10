ca4cff0a18b222b6839debeacbc028b02c7ce8d5
angeldollface <angeldollface666@gmail.com>
2023-06-10 16:02:06 +0200

    Tests still fail. #2

--- mod.ts
+++ mod.ts
@@ -1,32 +1,31 @@
-import type { Site } from "lume/core.ts";
-import sass from "lume/plugins/sass.ts";
+import type { Site } from 'lume/core.ts';
+import sass from 'lume/plugins/sass.ts';
 
 export default () => (site: Site) => {
-  // Add remote files
   const files = [
-    "_data/navigation.json",
-    "_data/site.json",
-    "_includes/layouts/blog.njk",
-    "_includes/layouts/page.njk",
-    "_includes/partials/head.njk",
-    "_includes/partials/heading.njk",
-    "_includes/partials/navigation.njk",
-    "_includes/sass/_desktop.scss",
-    "_includes/sass/_lume-dark-material.scss",
-    "_includes/sass/_responsive.scss",
-    "assets/logo.jpg",
-    "content/about.markdown",
-    "content/contact.markdown",
-    "css/index.scss",
-    "404.markdown",
-    "index.markdown",
+    '_data/navigation.json',
+    '_data/site.json',
+    '_includes/layouts/blog.njk',
+    '_includes/layouts/page.njk',
+    '_includes/partials/head.njk',
+    '_includes/partials/heading.njk',
+    '_includes/partials/navigation.njk',
+    '_includes/sass/_desktop.scss',
+    '_includes/sass/_lume-dark-material.scss',
+    '_includes/sass/_responsive.scss',
+    'assets/logo.jpg', 
+    'content/about.markdown',
+    'content/contact.markdown',
+    'css/index.scss',
+    '404.markdown',
+    'index.markdown',
   ];
 
   for (const file of files) {
-    site.remoteFile(file, import.meta.resolve(`./${file}`));
+    site.remoteFile(file, import.meta.resolve(file));
   }
 
   // Configure the site
   site.use(sass());
-  site.copy("assets");
+  site.copy('assets');
 };
